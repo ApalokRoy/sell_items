8323bf659f523167b22d3b932899d00a45ebbf6b
ApalokRoy <apalokroy5@gmail.com>
2019-08-10 12:34:36 +0530

    Conversation model & migrations updated

--- db/migrate/20190717094029_create_users.rb
+++ db/migrate/20190717094029_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :email, index: {unique: true}
-      t.string :password_digest
+      t.string :name, null: false, limit: 50
+      t.string :email, null: false, limit: 100, index: {unique: true}
+      t.string :password_digest, null: false
       t.boolean :admin, :default => false
       
       t.timestamps
--- db/migrate/20190719075721_create_conversations.rb
+++ db/migrate/20190719075721_create_conversations.rb
@@ -3,6 +3,8 @@ class CreateConversations < ActiveRecord::Migration[5.2]
     create_table :conversations do |t|
       t.references :sender, index: true, foreign_key: {to_table: :users}
       t.references :receiver, index: true, foreign_key: {to_table: :users}
+      t.references :post, index: true, foreign_key: true
+      t.index [:sender_id, :receiver_id, :post_id], unique: true
 
       t.timestamps
     end
