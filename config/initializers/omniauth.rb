06247658f2212402a83b2c862e6f3cf0fb41b034
ApalokRoy <apalokroy5@gmail.com>
2019-08-12 16:39:40 +0530

    omniauth initializer modified

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '287486472070349', '8e4f4a55a4dec74f907bc66c0bc88fcf'
-  provider :google_oauth2, 
-  '845097422285-keq7l5fgssqfn6qag6m3h8q976cu1t5m.apps.googleusercontent.com',
-  'cezj4VjEZTsKu4WPnu6XZVDz'
-
+  provider :facebook, Rails.application.secrets.FB_APP_ID, Rails.application.secrets.FB_APP_SECRET
+  provider :google_oauth2, Rails.application.secrets.GOOGLE_KEY, Rails.application.secrets.GOOGLE_SECRET
 end
